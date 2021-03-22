c5b7c8faacd072a483d419a535f00e4b8a859567
danger-ahead <danger.ahead@pm.me>
2021-03-23 00:41:07 +0530

    kicked off

---
+++ AboutMe.cpp
@@ -0,0 +1,9 @@
+//
+// Created by danger-ahead on 22/03/21.
+//
+
+#include "AboutMe.h"
+
+void AboutMe::display() {
+    printf("This shit is in another file. Fuck yeah!");
+}
---
+++ AboutMe.h
@@ -0,0 +1,14 @@
+//
+// Created by danger-ahead on 22/03/21.
+//
+
+#include <iostream>
+
+using namespace std;
+
+
+class AboutMe
+{
+public:
+    void display();
+};
